ff4b19d3795338a3df56522573a573c2cf2d0c2f
Daniel Crigan <crgdaniel@yahoo.com>
2025-03-29 21:58:57 +0000

    finish: CH4:Blocks by implementing Block to HTML logic

--- src/textnode.py
+++ src/textnode.py
@@ -32,10 +32,10 @@ def text_node_to_html_node(text_node):
         return LeafNode(None, text_node.text)
     if text_node.text_type == TextType.BOLD:
         return LeafNode("b", text_node.text)
-    if text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
     if text_node.text_type == TextType.ITALIC:
         return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
     if text_node.text_type == TextType.LINK:
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
--- tests/test_texttonodes.py
+++ tests/test_texttonodes.py
@@ -5,14 +5,10 @@ from src.utils import text_to_textnodes
 
 class TestTexToTextNode(unittest.TestCase):
     def test_text_to_textnodes(self):
-        node = TextNode(
-            "This is **text** with an _italic_ word and a `code block` "
-            "and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) "
-            "and a [link](https://boot.dev)",
-            TextType.TEXT,
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
         )
-        got = text_to_textnodes(node.text)
-        want = [
+        self.assertListEqual(
             [
                 TextNode("This is ", TextType.TEXT),
                 TextNode("text", TextType.BOLD),
@@ -21,15 +17,12 @@ class TestTexToTextNode(unittest.TestCase):
                 TextNode(" word and a ", TextType.TEXT),
                 TextNode("code block", TextType.CODE),
                 TextNode(" and an ", TextType.TEXT),
-                TextNode(
-                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
-                ),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
                 TextNode(" and a ", TextType.TEXT),
                 TextNode("link", TextType.LINK, "https://boot.dev"),
-            ]
-        ]
-
-        self.assertListEqual(got, want)
+            ],
+            nodes,
+        )
 
 
 if __name__ == "__main__":
