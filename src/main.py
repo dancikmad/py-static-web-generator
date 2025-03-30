f974f59b4b68bef9670ba0cab98ae9e03d0debe0
Daniel Crigan <crgdaniel@yahoo.com>
2025-03-30 02:25:22 +0100

    Updated main.sh / main.py to start a simple web server after static site gen

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from script import copy_static, logger
+from src.script import copy_static, logger, generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+
+page_dir_from = "./content/index.md"
+template_path = "./template.html"
+page_dir_to = "./public/index.html"
 
 
 def main():
@@ -9,6 +13,8 @@ def main():
     logger.info("Starting static file copy process")
     copy_static(dir_path_static, dir_path_public)
     logger.info("Static file copy completed")
+
+    generate_page(page_dir_from, template_path, page_dir_to)
 
 
 if __name__ == "__main__":
